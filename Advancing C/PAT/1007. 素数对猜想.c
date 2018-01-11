827c8fb469a82e8cca3be8b9eec62373829ec097
W-void <34032265+W-void@users.noreply.github.com>
2018-01-11 17:07:10 +0800

    Create 1007. 素数对猜想.c

---
+++ Advancing C/PAT/1007. 素数对猜想.c
@@ -0,0 +1,40 @@
+/*
+让我们定义 dn 为：dn = pn+1 - pn，其中 pi 是第i个素数。显然有 d1=1 且对于n>1有 dn 是偶数。“素数对猜想”认为“存在无穷多对相邻且差为2的素数”。
+现给定任意正整数N (< 105)，请计算不超过N的满足猜想的素数对的个数。
+
+输入格式：每个测试输入包含1个测试用例，给出正整数N。
+输出格式：每个测试用例的输出占一行，不超过N的满足猜想的素数对的个数。
+
+输入样例：
+20
+输出样例：
+4
+*/
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int issushu(int n)
+{
+	int m = sqrt(n);
+	int i;
+	for(i=2; i<=m; i++){
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+int judge(int n)
+{
+	if(issushu(n) && issushu(n+2))
+		return 1;
+	return 0;
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int i, num = 0, m;
+	for(i=3; i<=n-2; i++,i++){
+		if(judge(i)) num++;
+	}
+	printf("%d", num);
+}
