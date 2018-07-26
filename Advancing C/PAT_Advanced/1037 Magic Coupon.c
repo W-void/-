8d6ec7d9420a543e5f6a502362e2365cde300780
W-void <34032265+W-void@users.noreply.github.com>
2018-07-26 16:52:11 +0800

    Create 1037 Magic Coupon.c

---
+++ Advancing C/PAT_Advanced/1037 Magic Coupon.c
@@ -0,0 +1,42 @@
+/*
+1037 Magic Coupon (25)（25 分）提问
+
+The magic shop in Mars is offering some magic coupons. Each coupon has an integer N printed on it, meaning that when you use this coupon with a product, you may get N times the value of that product back! What is more, the shop also offers some bonus product for free. However, if you apply a coupon with a positive N to this bonus product, you will have to pay the shop N times the value of the bonus product... but hey, magically, they have some coupons with negative N's!
+For example, given a set of coupons {1 2 4 -1}, and a set of product values {7 6 -2 -3} (in Mars dollars M\$) where a negative value corresponds to a bonus product. You can apply coupon 3 (with N being 4) to product 1 (with value M\$7) to get M\$28 back; coupon 2 to product 2 to get M\$12 back; and coupon 4 to product 4 to get M\$3 back. On the other hand, if you apply coupon 3 to product 4, you will have to pay M\$12 to the shop.
+Each coupon and each product may be selected at most once. Your task is to get as much money back as possible.
+Input Specification:
+Each input file contains one test case. For each case, the first line contains the number of coupons NC, followed by a line with NC coupon integers. Then the next line contains the number of products NP, followed by a line with NP product values. Here 1<= NC, NP <= 10^5^, and it is guaranteed that all the numbers will not exceed 2^30^.
+Output Specification:
+For each test case, simply print in a line the maximum amount of money you can get back.
+Sample Input:
+4
+1 2 4 -1
+4
+7 6 -2 -3
+Sample Output:
+43
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int cmp(const void* a, const void* b)
+{
+  return *(int *)b - *(int *)a;
+}
+int main()
+{
+  int N, M, i, a[100010], b[100010];
+  scanf("%d", &N);
+  for(i=0; i<N; i++)
+    scanf("%d", a+i);
+  qsort(a, N, sizeof(int), cmp);
+  scanf("%d", &M);
+  for(i=0; i<M; i++)
+    scanf("%d", b+i);
+  qsort(b, M, sizeof(int), cmp);
+  int sum = 0;
+  for(i=0; i<N && i<M && a[i]>0 &&b[i]>0; i++)
+    sum += a[i]*b[i];
+  for(i=0; i<N && i<M && a[N-1-i]<0 && b[M-1-i]<0; i++)
+    sum += a[N-1-i]*b[M-1-i];
+  printf("%d", sum);
+}
