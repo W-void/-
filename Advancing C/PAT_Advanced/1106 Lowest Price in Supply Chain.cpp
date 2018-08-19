67c0005c010e1ba037311b782f62f1c340211a29
W-void <34032265+W-void@users.noreply.github.com>
2018-08-19 11:10:31 +0800

    用了DFS和BFS两种方法找深度最浅的叶子节点，BFS比DFS快一点

--- Advancing C/PAT_Advanced/1106 Lowest Price in Supply Chain.cpp
+++ Advancing C/PAT_Advanced/1106 Lowest Price in Supply Chain.cpp
@@ -1,3 +1,4 @@
+// 用了DFS和BFS两种方法找深度最浅的叶子节点，BFS比DFS快一点
 /*
 1106. Lowest Price in Supply Chain (25)
 
@@ -47,13 +48,14 @@ Sample Output:
 #include<cstdio>
 #include<vector>
 #include<cmath>
+#include<queue>
 using namespace std;
 
 int n;
 double p, r;
 const int maxn = 100010;
 vector<int> node[maxn];
-int depth = maxn, num;
+int depth = maxn, num = 0;
 
 void DFS(int root, int dep)
 {
@@ -72,6 +74,32 @@ void DFS(int root, int dep)
             DFS(node[root][i], dep+1);
     }
 }
+void BFS(int root, int dep)
+{
+    queue<int> q;
+    q.push(root);
+    int cnt = 1, j = 0;
+    while(!q.empty()){
+        root = q.front();
+        int size = node[root].size(), i;
+        for(i=0; i<size; i++){
+            q.push(node[root][i]);
+        }
+        if(size == 0) //到达叶子节点，计数器加一
+            num++;
+        q.pop();
+        j++;
+        if(j == cnt){ //遍历完完整的一层
+            cnt = q.size();
+            j = 0;
+            if(num){ // 找到叶子节点
+                depth = dep;
+                break;
+            }
+            dep++;
+        }
+    }
+}
 int main()
 {
     scanf("%d%lf%lf", &n, &p, &r);
@@ -83,6 +111,7 @@ int main()
             node[i].push_back(tmp);
         }
     }
-    DFS(0, 0);
+    //DFS(0, 0);
+    BFS(0, 0);
     printf("%.4f %d", pow(1+r/100, depth) * p, num);
 }
